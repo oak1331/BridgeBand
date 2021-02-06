46a2957df3d33e9ea4a4ed4963e7ccd7a193e452
oak1331 <oaks1331@gmail.com>
2021-02-07 05:48:41 +0900

    画像表示の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,10 @@
 class TweetsController < ApplicationController
   def index
   end
+
+  private
+
+  def message_params
+    params.require(:tweet).permit(:text, :image).merge(user_id: current_user.id)
+  end
 end
