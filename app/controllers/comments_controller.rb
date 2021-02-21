e3b3c7c56b240b6d69e75aa733e799e056ada28c
oak1331 <oaks1331@gmail.com>
2021-02-22 05:22:11 +0900

    createアクションの定義

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.create(comment_params)
+    if @comment.save
+      redirect_to tweet_path(@comment.tweet)
+    else
+      @prototype = @comment.tweet
+      @comments = @prototype.comments
+      render "tweets/show"
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text ).merge(user_id: current_user.id, tweet_id: params[:tweet_id] )
+  end
 end
