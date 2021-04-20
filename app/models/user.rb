537b4574ca7ea1deb1bc55105e28239fca7a69e8
oak1331 <oaks1331@gmail.com>
2021-04-21 06:37:04 +0900

    アソシエーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ApplicationRecord
   has_many :tweets
   has_many :comments
   has_many :favorites
+  has_many :relationships, dependent: :destroy
+  has_many :followings, through: :relationships, source: :follower
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :user 
   
   validates :name, presence: true
 
